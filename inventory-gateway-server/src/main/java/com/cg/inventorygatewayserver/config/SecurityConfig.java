891095ee141f539e7470d35f47b531b02d629923
Santosh S. Kudale <FWIN01874@fulcrumdigital.com>
2021-07-13 13:46:34 +0530

    Signed-off-by: Santosh S. Kudale CORS changes

--- inventory-gateway-server/src/main/java/com/cg/inventorygatewayserver/config/SecurityConfig.java
+++ inventory-gateway-server/src/main/java/com/cg/inventorygatewayserver/config/SecurityConfig.java
@@ -1,89 +1,66 @@
-/**
- * @author Santosh Kudale
- * @email santosh.kudale96@gmail.com
- * @create date 2020-10-29 16:50:12
- * @modify date 2020-10-29 16:50:12
- * @desc Security COnfiguration
- */
 package com.cg.inventorygatewayserver.config;
 
-import java.util.Arrays;
-
 import com.cg.inventorygatewayserver.security.CustomAuthenticationEntryPoint;
-import com.cg.inventorygatewayserver.security.JwtAuthenticationFilter;
-import com.cg.inventorygatewayserver.service.implementation.JwtUserDetailsServiceImpl;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import javax.servlet.Filter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-
-import lombok.AllArgsConstructor;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import com.cg.inventorygatewayserver.service.implementation.JwtUserDetailsServiceImpl;
+import com.cg.inventorygatewayserver.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-@AllArgsConstructor
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	private JwtUserDetailsServiceImpl userDetailsService;
 
-  private JwtAuthenticationFilter jwtAuthenticationFilter;
-  private JwtUserDetailsServiceImpl userDetailsService;
+	protected void configure(final HttpSecurity http) throws Exception {
+		((HttpSecurity) ((HttpSecurity) ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl) ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl) ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl) ((HttpSecurity) http
+				.csrf().disable())
+						.authorizeRequests()
+						.antMatchers(new String[] { "/inventory-auth-service/**", "/actuator/**", "/**/h2/**",
+								"/**/swagger*/**", "/**/v2/api-docs" ,"/*"})).permitAll()
+										.antMatchers(new String[] { "/h2" })).permitAll().anyRequest()).authenticated()
+												.and()).sessionManagement()
+														.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and())
+																.exceptionHandling().authenticationEntryPoint(
+																		this.authenticationEntryPoint());
+		http.addFilterBefore((Filter) this.jwtAuthenticationFilter, (Class) UsernamePasswordAuthenticationFilter.class);
+		http.headers().frameOptions().disable();
+	}
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-    .cors()
-    .configurationSource(request -> {
-        CorsConfiguration source = new CorsConfiguration();
-        source.applyPermitDefaultValues();
-        // .applyPermitDefaultValues(); only allows GET, HEAD, POST
-        source.setAllowedMethods(Arrays.asList("GET", "HEAD", "POST", "DELETE", "PUT"));
-      return source;
-      }).and() // Required for accessing prpotected routes
-      .csrf().disable()
-      .authorizeRequests().antMatchers("/inventory-auth-service/**", "/actuator/**", "/**/h2/**", "/**/swagger*/**", "/**/v2/api-docs").permitAll().antMatchers("/h2").permitAll()
-      .anyRequest().authenticated()
-      .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-      .and().exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
-    http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-    http.headers().frameOptions().disable();
-  }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return (PasswordEncoder) new BCryptPasswordEncoder();
+	}
 
-  /**
-   * Password Encodeer DI
-   */
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService((UserDetailsService) this.userDetailsService).passwordEncoder(this.passwordEncoder());
+	}
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-  }
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
-  /**
-   * In @Bean(BeanIds.AUTHENTICATION_MANAGER) NeanIds part is optional
-   */
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
+	public AuthenticationEntryPoint authenticationEntryPoint() {
+		return (AuthenticationEntryPoint) new CustomAuthenticationEntryPoint();
+	}
 
-  /**
-   * DI for CustomAuthentiaction EntryPoint
-   */
-  public AuthenticationEntryPoint authenticationEntryPoint() {
-    return new CustomAuthenticationEntryPoint();
-  }
-
+	public SecurityConfig(final JwtAuthenticationFilter jwtAuthenticationFilter,
+			final JwtUserDetailsServiceImpl userDetailsService) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+		this.userDetailsService = userDetailsService;
+	}
 }
